64867cbf8dd58b2597f48750563e12eac0eadafe
Daniel Lowrey <rdlowrey@php.net>
2014-02-16 23:34:53 -0500

    PHP 5.4 requirement obviates need for test annotations

--- test/Arya/Test/IntegrationTest.php
+++ test/Arya/Test/IntegrationTest.php
@@ -9,98 +9,65 @@ class IntegrationTest extends \PHPUnit_Framework_TestCase {
     private static $client;
     private static $baseUri;
 
-    /**
-     * @requires PHP 5.4
-     */
     public static function setupBeforeClass() {
         self::$client = new \Artax\Client;
         self::$baseUri = sprintf('http://%s:%d', WEB_SERVER_HOST, WEB_SERVER_PORT);
     }
 
-    /**
-     * @requires PHP 5.4
-     */
     public function testFunctionTarget() {
         $uri = self::$baseUri . '/test-function-target';
         $response = self::$client->request($uri);
         $this->assertEquals('test', $response->getBody());
     }
 
-    /**
-     * @requires PHP 5.4
-     */
     public function testLambdaTarget() {
         $uri = self::$baseUri . '/test-lambda-target';
         $response = self::$client->request($uri);
         $this->assertEquals('test', $response->getBody());
     }
 
-    /**
-     * @requires PHP 5.4
-     */
     public function testStaticTarget() {
         $uri = self::$baseUri . '/test-static-target';
         $response = self::$client->request($uri);
         $this->assertEquals('test', $response->getBody());
     }
 
-    /**
-     * @requires PHP 5.4
-     */
     public function testInstanceMethodTarget() {
         $uri = self::$baseUri . '/test-instance-method-target';
         $response = self::$client->request($uri);
         $this->assertEquals('2 | 1', $response->getBody());
     }
 
-    /**
-     * @requires PHP 5.4
-     */
     public function testRouteArgs() {
         $uri = self::$baseUri . '/arg1/arg2/42';
         $response = self::$client->request($uri);
         $this->assertEquals('arg1 | arg2 | 42 | arg1 | arg2 | 42', $response->getBody());
     }
 
-    /**
-     * @requires PHP 5.4
-     */
     public function testRouteArgsUrlDecodedByDefault() {
         $uri = self::$baseUri . '/arg1/%3Ctest%3E/42';
         $response = self::$client->request($uri);
         $this->assertEquals('arg1 | <test> | 42 | arg1 | <test> | 42', $response->getBody());
     }
 
-    /**
-     * @requires PHP 5.4
-     */
     public function test404OnUnmatchedNumericRouteArg() {
         $uri = self::$baseUri . '/arg1/arg2/should-be-numeric-but-isnt';
         $response = self::$client->request($uri);
         $this->assertEquals(404, $response->getStatus());
     }
 
-    /**
-     * @requires PHP 5.4
-     */
     public function test404OnUnmatchedRoute() {
         $uri = self::$baseUri . '/some-route-that-clearly-doesnt-exist';
         $response = self::$client->request($uri);
         $this->assertEquals(404, $response->getStatus());
     }
 
-    /**
-     * @requires PHP 5.4
-     */
     public function test500OnTargetOutput() {
         $uri = self::$baseUri . '/generates-output';
         $response = self::$client->request($uri);
         $this->assertEquals(500, $response->getStatus());
     }
 
-    /**
-     * @requires PHP 5.4
-     */
     public function testComplexResponse() {
         $uri = self::$baseUri . '/complex-response';
         $response = self::$client->request($uri);
