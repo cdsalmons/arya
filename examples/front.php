b94f8a05e00ea1a617e2496a9be65253c27cfb7d
Daniel Lowrey <rdlowrey@php.net>
2014-02-20 13:17:35 -0500

    Update examples

--- examples/front.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$__application = __DIR__ . '/app.php';
-$__debug = TRUE;
-
-require __DIR__ . '/../arya.php';
---
+++ examples/front_controller.php
@@ -0,0 +1,10 @@
+<?php
+
+// The absolute path to your application file
+define('APPLICATION', __DIR__ . '/my_app.php');
+
+// Turn debug mode on or off (optional, defaults to TRUE)
+define('DEBUG', TRUE);
+
+// The absolute path to the arya bootstrap file
+require __DIR__ . '/../src/arya.php';
