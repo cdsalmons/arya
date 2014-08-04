dc6c2670bd50e9540adfd3b8a367568932bdc0e5
ronnieskansing <rskansing@gmail.com>
2014-08-04 23:56:33 +0200

    array() to short array syntax []

--- lib/Sessions/Session.php
+++ lib/Sessions/Session.php
@@ -14,11 +14,11 @@ class Session implements \ArrayAccess, \Iterator {
     private $request;
     private $handler;
     private $sessionId;
-    private $data = array();
+    private $data = [];
     private $isOpen = FALSE;
     private $needsSave = FALSE;
     private $wasAltered = FALSE;
-    private $options = array(
+    private $options = [
         'cookie_name' => 'ASESSID',
         'cookie_domain' => '',
         'cookie_path' => '',
@@ -35,7 +35,7 @@ class Session implements \ArrayAccess, \Iterator {
         'gc_divisor' => 100,
         'gc_max_lifetime' => 1440,
         'strict' => TRUE
-    );
+    ];
 
     public function __construct(Request $request, SessionHandler $handler = NULL) {
         $this->request = $request;
@@ -390,7 +390,7 @@ class Session implements \ArrayAccess, \Iterator {
         if ($oldSessionId = $this->sessionId) {
             $this->handler->close();
             $this->handler->destroy($oldSessionId);
-            $this->data = array();
+            $this->data = [];
             $this->isOpen = FALSE;
         }
 
@@ -403,17 +403,17 @@ class Session implements \ArrayAccess, \Iterator {
     }
 
     public function getCookieElements() {
-        return array(
+        return [
             $this->options['cookie_name'],
             $this->sessionId,
-            array(
+            [
                 'domain' => $this->options['cookie_domain'],
                 'path' => $this->options['cookie_path'],
                 'expire' => $this->options['cookie_lifetime'],
                 'secure' => $this->options['cookie_secure'],
                 'httponly' => $this->options['cookie_httponly']
-            )
-        );
+            ]
+        ];
     }
 
     public function offsetSet($offset, $value) {
